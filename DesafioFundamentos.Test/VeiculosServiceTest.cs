5177f1c4166c7103f3f27970a0bac53b1e7c054e
João Filho <joaofilholimati@gmail.com>
2025-07-18 11:47:58 -0300

    test: adicionando teste unitário para o metódo VeiculosService

---
+++ DesafioFundamentos.Test/VeiculosServiceTest.cs
@@ -0,0 +1,23 @@
+﻿
+using DesafioFundamentos.Services;
+
+namespace DesafioFundamentos.Test
+{
+    public class VeiculosServiceTest
+    {
+        [Fact]
+        public void DeveRetornarListaComVeiculos()
+        {
+            // Arrange
+            var veiculos = new List<string> { "ASD1234", "ABC1234" };
+            var service = new VeiculosService();
+
+            // Act
+            var resultado = service.ListarVeiculos(veiculos);
+
+            // Assert
+            Assert.Equal(2, resultado.Count);
+            Assert.Contains("ASD1234", resultado);
+        }
+    }
+}
